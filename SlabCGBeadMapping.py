26ac9570b3b7f7e099065f61afc07b506a0bfb89
mikhail-a-ivanov <mikhail.ivanov.1995@gmail.com>
2019-11-11 16:22:56 +0100

    implemented the distance-based bead mapping algorithm

--- SlabCGBeadMapping.py
+++ SlabCGBeadMapping.py
@@ -1,19 +1,13 @@
 import numpy as np
 from timeit import default_timer as timer
 from IO import writeGRO
-
-from beadMapping import CGBeadMappingMain
+from beadMapping import CGBeadMappingMain, CGbeadHistogram
 
 start = timer()
-AllDistances = CGBeadMappingMain('anatase-101-POPE.gro', '1H151', 16, 16, 9)
-
-closest_atoms = np.argsort(AllDistances, axis=None)[:13910]
-
-print(closest_atoms // 13910)
-print(closest_atoms % 13910)
-
-
+AllCGBead_AtomIndexes = CGBeadMappingMain('anatase-101-POPE.gro', '1H151', 12, 12, 9)
 end = timer()
+
+CGbeadHistogram(AllCGBead_AtomIndexes, 100)
 
 print('Bead mapping took',round(end - start, 4), ' seconds.')
 
--- beadMapping.py
+++ beadMapping.py
@@ -1,22 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from slabInit import Initialize
 
 def CGBeadMappingMain(filename, resname, x_points, y_points, z_points):
     
     atoms, CGbeads, spacings = Initialize(filename, resname, x_points, y_points, z_points)
     
-    """ 
-    The idea: 
-    0. Create AllCGBead_AtomIndexes
-    1. Pick an atom (atoms[atom_number])
-    2. Calculate distances between atom and all CG bead centers
-    3. Do np.argsort on the distances, pick the first number from np.argsort(distances) - that will
-    be the index of the closest CG bead (CGbeads[CGbead_index])!
-    5. Start building CGBead_AtomIndexes: AllCGbead_AtomIndexes[CGbead_index].append(atom_number)
-    ???
-    """
-    
+    # Creating a list of atom indeces that are assigned to a certain CG bead
     AllCGBead_AtomIndexes = []
+    for i in range(len(CGbeads)):
+        AllCGBead_AtomIndexes.append([])
     
     # Build a matrix that contains len(CGbeads) repeats of the atomic coordinates
     atoms_matrix = np.repeat(atoms, len(CGbeads), axis=0)
@@ -29,11 +22,35 @@ def CGBeadMappingMain(filename, resname, x_points, y_points, z_points):
     CGbeads_matrix = np.repeat([CGbeads], len(atoms), axis=0)
     
     # Calculate all distances
+    print("Calculating distances between every atom and all CG beads...")
     AllDistances = np.linalg.norm(atoms_matrix - CGbeads_matrix, axis=2)
-    print("Distances between every atom and all CG beads have been calculated.\n")
-
-
-
-
     
-    return(AllDistances)    
+    # Getting global (or flat) indices of N (N = len(atoms)) shortest distances
+    print("Sorting distances...")
+    closestAtoms = np.argsort(AllDistances, axis=None)[:len(atoms)]
+
+    # We can get the atom index and CG bead index from the global (flat) index of the distance
+    # By modulo operation we get the atom index, by floor division we get the CG bead index,
+    # that are separated by the calculated distance
+    atomIndices = closestAtoms % len(atoms)
+    CGbeadIndices = closestAtoms // len(atoms)
+
+    for i in range(len(atoms)):
+        AllCGBead_AtomIndexes[CGbeadIndices[i]].append(atomIndices[i])
+
+    return AllCGBead_AtomIndexes
+
+def CGbeadHistogram(AllCGbead_AtomIndexes, Nbins):
+    atoms_in_CGbead = []
+    for CGbead in AllCGbead_AtomIndexes:
+        atoms_in_CGbead.append(len(CGbead))
+    atoms_in_CGbead = np.array(atoms_in_CGbead)
+    fig = plt.figure(figsize=(12,7))
+    plt.ylabel('Count', size=20)
+    plt.xlabel('Number of atoms per CG bead', size=20)
+    plt.grid(b=True, which='major' , linestyle='--', color= 'grey', linewidth=1, zorder=1)
+    plt.grid(b=True, which='minor', linestyle='--', color= 'grey', linewidth=1, zorder=1)
+    plt.hist(atoms_in_CGbead, bins = Nbins)
+    plt.show()
+    
+    return 
