354137c59bdc8f6d61a7613881a290858635adf4
mikhail-a-ivanov <mikhail.ivanov.1995@gmail.com>
2020-10-22 21:04:33 +0200

    added working slabCGanalyse module

--- IO.py
+++ IO.py
@@ -1,4 +1,81 @@
 import numpy as np
+#from slabCGanalyze import 
+
+A_to_nm = 0.1
+
+def readCGSlab(conf_file, header_rows = 2):
+    """
+    Opens and reads input configuration file with the 
+    CG slab geometry, returns list of lines
+    with bead coordinates, the total number of beads
+    and the box vectors (nm)
+    """
+    lines = []
+    try:
+        with open(conf_file, 'r') as file:
+            for row in file:
+                lines.append(row.split())
+    except IOError:
+        print(f'Could not read the file {conf_file}!')
+
+    # Save the total number of beads
+    try:
+        NBeads = int(lines[0][0])
+    except:
+        print('Could not read the number of beads!')
+
+    # Save the box vectors
+    try:
+        box = [float(lines[1][3]) * A_to_nm, float(lines[1][4]) * A_to_nm, float(lines[1][5]) * A_to_nm]
+    except:
+        print('Could not read the box vectors!') 
+
+    # Remove header
+    assert len(lines) != 0, 'Could not read bead coordinates!'
+
+    del(lines[0:header_rows])
+
+    assert len(lines) == NBeads, 'Number of lines in the file does not match the total number of beads!'
+
+    return lines, NBeads, box
+
+
+def buildCGSlab(beadType1='TiA', beadType2='TiB', area=49, width=3, originalSlabGeometry='anatase-101/last_frame.xmol'):
+
+    CGslabFile, NBeads, box = readCGSlab(originalSlabGeometry)
+
+
+    return
+
+
+def writeCGSlabGRO(CGbeads, filename, resname, atomname):
+    CGbeads = np.around(CGbeads, decimals=3)
+    
+    with open(filename, 'r') as file:
+        lines = file.read().splitlines()
+        pbc = lines[-1].split()
+    
+    
+    filename = resname + "_" + str(len(CGbeads)) + "CGbeads.gro"
+    file_header = "resname: " + resname + "; " + str(len(CGbeads))+ " " + "CG beads" + "\n" + str(len(CGbeads))
+    
+    gro_file = open(filename, 'w+')
+    gro_file.write(file_header + '\n')
+    
+    for CGbead_index in range(len(CGbeads)):
+        gro_file.write(format(resname).rjust(9))
+        gro_file.write(format(atomname + str(CGbead_index+1)).rjust(6))
+        gro_file.write(format(str(CGbead_index+1)).rjust(5))
+        for axis_index in range(len(CGbeads[CGbead_index])):
+            gro_file.write(format(CGbeads[CGbead_index][axis_index], '#.3f').rjust(8))
+        gro_file.write("\n")
+    
+    for axis_index in range(len(pbc)):
+        gro_file.write(format(float(pbc[axis_index]), '#.5f').rjust(10))
+    gro_file.write("\n")
+    gro_file.close()
+    print(filename, "saved.")
+    return
 
 # This function reads the gro file, looks for all the strings with that contain the name of the slab residue
 # and save the data to the output in the format: ['Atom name and atom number', 'atom number', 'X', 'Y', 'Z']
@@ -36,35 +113,5 @@ def readGRO(filename, resname):
             atom[coordinate] = float(atom[coordinate])
            
     return(np.array(slab_gmx))
-
-
-def writeGRO(CGbeads, filename, resname, atomname):
-    CGbeads = np.around(CGbeads, decimals=3)
-    
-    with open(filename, 'r') as file:
-        lines = file.read().splitlines()
-        pbc = lines[-1].split()
-    
-    
-    filename = resname + "_" + str(len(CGbeads)) + "CGbeads.gro"
-    file_header = "resname: " + resname + "; " + str(len(CGbeads))+ " " + "CG beads" + "\n" + str(len(CGbeads))
-    
-    gro_file = open(filename, 'w+')
-    gro_file.write(file_header + '\n')
-    
-    for CGbead_index in range(len(CGbeads)):
-        gro_file.write(format(resname).rjust(9))
-        gro_file.write(format(atomname + str(CGbead_index+1)).rjust(6))
-        gro_file.write(format(str(CGbead_index+1)).rjust(5))
-        for axis_index in range(len(CGbeads[CGbead_index])):
-            gro_file.write(format(CGbeads[CGbead_index][axis_index], '#.3f').rjust(8))
-        gro_file.write("\n")
-    
-    for axis_index in range(len(pbc)):
-        gro_file.write(format(float(pbc[axis_index]), '#.5f').rjust(10))
-    gro_file.write("\n")
-    gro_file.close()
-    print(filename, "saved.")
-    return
     
     
--- slabCGanalyze.py
+++ slabCGanalyze.py
@@ -1,34 +1,13 @@
 import numpy as np
+from IO import readCGSlab
 
-def readCGSlab(mmol_file, header_rows = 2):
-    # Open the mmol file with the CG slab geometry
-    lines = []
-    try:
-        with open(mmol_file, 'r') as file:
-            for row in file:
-                lines.append(row.split())
-    except IOError:
-        print(f'Could not read the file {mmol_file}!')
-
-    # Save the total number of beads
-    try:
-        NBeads = int(lines[0][0])
-    except:
-        print('Could not read the number of beads!')
-
-    # Remove header
-    assert len(lines) != 0, 'Could not read bead coordinates!'
-
-    del(lines[0:header_rows])
-
-    assert len(lines) != 0, 'Could not read bead coordinates!'
-
-    return lines, NBeads
-
+A_to_nm = 0.1
 
 def getBeadTypes(lines):
-    # Read CG bead names, remove numbers and 
-    # save unique CG bead types in the beadTypes list
+    """
+    Reads CG bead names, removes numbers from the names and 
+    save unique CG bead types in the beadTypes list
+    """
     beadNames = []
     for line in lines:
         beadNames.append(''.join([i for i in line[0] if not i.isdigit()]))
@@ -41,8 +20,11 @@ def getBeadTypes(lines):
 
 
 def beadCoordinates(lines, beadType):
-    # Return coordinates of all beads
-    # of the input type in a numpy array
+    """
+    Returns the coordinates (nm) of all beads
+    of the input type in a numpy array
+    """
+    assert type(beadType) == str, 'Could not read bead type!'
 
     beadCoordinates = []
 
@@ -50,7 +32,7 @@ def beadCoordinates(lines, beadType):
         if beadType in line[0]:
              beadCoordinates.append([float(line[1]), float(line[2]), float(line[3])])
 
-    beadCoordinates = np.array(beadCoordinates)
+    beadCoordinates = np.array(beadCoordinates) * A_to_nm
 
     assert len(beadCoordinates) != 0, 'Could not read bead coordinates!'
 
@@ -59,11 +41,21 @@ def beadCoordinates(lines, beadType):
     return beadType, beadCoordinates
 
 
-def averageMaxSeparation(beadCoordinates, axis=2):
+def getBeadSeparation(beadCoordinates, axis=2):
+    """
+    Returns average separation (nm) along one axis
+    between two groups of the same bead type
+    in the opposite planes of the slab
+
+    For the surfacemost beads gives the slab width
+    """
+
+    assert type(beadCoordinates) == np.ndarray, 'Could not read bead coordinates!'
+
     # Calculate the mean coordinate along one axis
     meanCoordinate = np.mean(beadCoordinates, axis=0)[axis]
 
-    # Divide bead coordinates into two groups:
+    # Divides bead coordinates into two groups:
     # 1. coordinate < mean coordinate
     # 2. coordinate > mean coordinate
     minCoordinates = beadCoordinates[beadCoordinates.T[axis] < meanCoordinate]
@@ -71,22 +63,68 @@ def averageMaxSeparation(beadCoordinates, axis=2):
 
     assert len(minCoordinates) + len(maxCoordinates) == len(beadCoordinates), 'Number of beads with min and max coordinate does not add up!'
 
-    return minCoordinates, maxCoordinates
+    BeadSeparation = np.mean(maxCoordinates.T[axis]) - np.mean(minCoordinates.T[axis])
+
+    assert BeadSeparation > 0, 'Could not calculate the average separation!'
+
+    return BeadSeparation
+
+
+def getBeadTypeSeparation(beadCoordinates1, beadCoordinates2, axis=2):
+    """
+    Returns average separation between two
+    bead types along one direction using the
+    following algorithm:
+
+    Slab schematics:
+    BeadType1 --- BeadType2 --- --- BeadType2 --- BeadType1
+
+    Separation between two bead types:
+    abs(getBeadSeparation(beadCoordinates1) - getBeadSeparation(beadCoordinates2)) / 2
+    """
+
+    assert type(beadCoordinates1) == np.ndarray, 'Could not read bead 1 coordinates!'
+    assert type(beadCoordinates2) == np.ndarray, 'Could not read bead 2 coordinates!'
+
+    separation = (getBeadSeparation(beadCoordinates1, axis=2) - getBeadSeparation(beadCoordinates2, axis=2)) / 2
+
+    return separation
+
+def getBeadsPerArea(beadCoordinates, box, axis=2):
+    """Calculates the average number of beads per area.
+    Input axis is normal to the slab 
+    """
+    dimensions = [0, 1, 2]
+    dimensions.remove(axis)
+
+    assert len(dimensions) == 2, 'Could not select the plane!'
+
+    beadsPerArea = len(beadCoordinates) / (box[dimensions[0]] * box[dimensions[1]])
+
+    return beadsPerArea
 
 
 
 
 
 
-CGslabFile, NBeads = readCGSlab('anatase-101/a101.CG.mmol')
-beadTypes = getBeadTypes(CGslabFile)
-print(NBeads)
-print(beadTypes)
-beadType, beadCoordinates = beadCoordinates(CGslabFile, beadTypes[0])
-print(beadType)
-print(beadCoordinates)
-print(beadCoordinates.shape)
+CGslabFile, NBeads, box = readCGSlab('anatase-101/last_frame.xmol')
+print(box)
 
-z_min, z_max = averageMaxSeparation(beadCoordinates, axis=2)
-#print(z_min)
-#print(z_max)
+beadType1, coordinates1 = beadCoordinates(CGslabFile, 'TiA')
+beadType2, coordinates2 = beadCoordinates(CGslabFile, 'TiB')
+
+width_TiA = getBeadSeparation(coordinates1, axis=2)
+print(width_TiA)
+
+width_TiB = getBeadSeparation(coordinates2, axis=2)
+print(width_TiB)
+
+separation = getBeadTypeSeparation(coordinates1, coordinates2)
+print(separation)
+
+beadsPerArea1 = getBeadsPerArea(coordinates1, box, axis=2)
+print(beadsPerArea1)
+
+beadsPerArea2 = getBeadsPerArea(coordinates2, box, axis=2)
+print(beadsPerArea2)
